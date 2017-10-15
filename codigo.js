d7467057a13164a16b4ca0706ca4f665af027fac
HNoa <hnoa3838@gmail.com>
2017-10-15 11:53:13 -0400

    primer commit

---
+++ codigo.js
@@ -0,0 +1,20 @@
+function mostrar(){
+  /*
+  var n = document.form.nombre.value;
+  var c = document.form.correo.value;
+  var nn = document.form.numero.value;
+  if(n&&c&&nn){
+    //alert("Correcto");
+    //var p = document.main.createElement('p');
+    //document.main.write("<p>"+n+"</p>");
+    //var p = document.main.createElement('p');
+    //document.main.write(c);
+    //var p = document.main.createElement('p');
+    //document.main.write(nn);
+  }
+  else {
+    //alert("Incorrecto");
+  }*/
+  alert('Hola');
+  //document.getElementById("insertanombre").value = document.getElementById("nombre").value;
+}
