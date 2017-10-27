d6eb911fba159fd4903d7f6b0af5322799f0346a
HNoa <hnoa3838@gmail.com>
2017-10-27 16:58:09 -0400

    mejorado

--- codigo.js
+++ codigo.js
@@ -3,10 +3,24 @@ function mostrar(){
   var c = document.form.correo.value;
   var nn = document.form.numero.value;
   if(n&&c&&nn){
-    var parrafo = document.createElement('p');
-    var texto = document.createTextNode("Nombre: "+n+"    Correo: "+c+"     Número: "+nn);
-    parrafo.appendChild(texto);
-    document.body.appendChild(parrafo);
+    var p1 = document.createElement('p');
+    var t1 = document.createTextNode("Nombre: "+n);
+    var linea = document.createElement('br');
+    p1.appendChild(t1);
+    document.body.appendChild(p1);
+    document.body.appendChild(linea);
+
+    var p2 = document.createElement('p');
+    var t2 = document.createTextNode("Correo: "+c);
+    p2.appendChild(t2);
+    document.body.appendChild(p2);
+    document.body.appendChild(linea);
+
+    var p3 = document.createElement('p');
+    var t3 = document.createTextNode("Número: "+nn);
+    p3.appendChild(t3);
+    document.body.appendChild(p3);
+    document.body.appendChild(linea);
   }
   else {
     alert("Incorrecto");
