83568ae9212f7ffbd370d4041262dc563bba2512
HNoa <hnoa3838@gmail.com>
2017-10-27 16:17:28 -0400

    mejorar

--- codigo.js
+++ codigo.js
@@ -1,20 +1,14 @@
 function mostrar(){
-  /*
   var n = document.form.nombre.value;
   var c = document.form.correo.value;
   var nn = document.form.numero.value;
   if(n&&c&&nn){
-    //alert("Correcto");
-    //var p = document.main.createElement('p');
-    //document.main.write("<p>"+n+"</p>");
-    //var p = document.main.createElement('p');
-    //document.main.write(c);
-    //var p = document.main.createElement('p');
-    //document.main.write(nn);
+    var parrafo = document.createElement('p');
+    var texto = document.createTextNode("Nombre: "+n+"    Correo: "+c+"     Número: "+nn);
+    parrafo.appendChild(texto);
+    document.body.appendChild(parrafo);
   }
   else {
-    //alert("Incorrecto");
-  }*/
-  alert('Hola');
-  //document.getElementById("insertanombre").value = document.getElementById("nombre").value;
+    alert("Incorrecto");
+  }
 }
